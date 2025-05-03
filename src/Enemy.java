b721c6d71cae2f61ed302b6356944acf6d582a81
Pho Anh Khoi Huynh <kenhuynhlx1@gmail.com>
2025-05-04 00:18:37 +0700

    Add Enemy class and update enemy stats

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,3 +1,27 @@
+import java.util.Random;
+
 public class Enemy {
+String name;
+int healthPoints, armour, initiative;
+double currencyDrop, experienceDrop;
+Ability attack1;
+public Enemy(String name,int healthPoints, int armour,int initiative,double currencyDrop, double experienceDrop ){
+    this.name = name;
+    this.healthPoints= healthPoints;
+    this.armour=armour;
+    this. currencyDrop=currencyDrop;
+    this.experienceDrop=experienceDrop;
+    this.initiative=initiative;
+    
+}
+ public int rollInitiative() {
+        Random rand = new Random();
+        int roll = rand.nextInt(this.initiative);
+        return roll;
+    }
+    
+    public String toString() {
+        return "Enemy(" + name + ")";
+    }
 
 }
